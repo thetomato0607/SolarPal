8db2aebbea6d31c7da15ad6f007e991b69e15aa1
thetomato0607 <yuilonlam0607@gmail.com>
2025-08-26 02:36:25 +0100

    chore: remove unused imports

--- solarpal-frontend/src/components/CloudBackground.jsx
+++ solarpal-frontend/src/components/CloudBackground.jsx
@@ -1,6 +1,3 @@
-import Sun from "./ui/Sun";
-import HouseGrid from "./HouseGrid";
-
 export default function CloudBackground({ children }) {
   return (
     <div style={{ minHeight: "100dvh", position: "relative" }}>
